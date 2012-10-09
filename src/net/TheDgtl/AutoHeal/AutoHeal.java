d5a3e4fc2addf9cba7000fd94cf3e413dc892848
Steven Scott <Contact@TheDgtl.net>
2012-10-08 19:26:02 -0700

    Allow potions/food to heal again

--- src/net/TheDgtl/AutoHeal/AutoHeal.java
+++ src/net/TheDgtl/AutoHeal/AutoHeal.java
@@ -13,7 +13,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -136,17 +135,6 @@ public class AutoHeal extends JavaPlugin {
 	}
 	
 	public class entityListener implements Listener {
-		/**
-		 * Disable food healing, no point with AutoHeal enabled
-		 */
-		@EventHandler
-		public void onEntityRegainHealth(EntityRegainHealthEvent event) {
-			if (event.isCancelled()) return;
-			if (!(event.getEntity() instanceof Player)) return;
-			
-			event.setCancelled(true);
-		}
-		
 		/**
 		 * Cancel starvation events if disabled on this world
 		 */
